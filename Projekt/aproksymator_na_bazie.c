6bcdfd863ba28cea9c5e12f8ca6f720af26c84bd
wawrzynd <wawrzynd@ee.pw.edu.pl>
2020-01-22 03:55:07 +0100

    Wielomiany Hermite'a

--- Projekt/aproksymator_na_bazie.c
+++ Projekt/aproksymator_na_bazie.c
@@ -14,102 +14,47 @@
  * - numer funkcji x - wspolrzedna dla ktorej obliczana jest wartosc funkcji
  */
 double
-fi(double a, double b, int n, int i, double x)
-{
-	double		h = (b - a) / (n - 1);
-	double		h3 = h * h * h;
-	int		hi         [5] = {i - 2, i - 1, i, i + 1, i + 2};
-	double		hx      [5];
-	int		j;
-
-	for (j = 0; j < 5; j++)
-		hx[j] = a + h * hi[j];
-
-	if ((x < hx[0]) || (x > hx[4]))
-		return 0;
-	else if (x >= hx[0] && x <= hx[1])
-		return 1 / h3 * (x - hx[0]) * (x - hx[0]) * (x - hx[0]);
-	else if (x > hx[1] && x <= hx[2])
-		return 1 / h3 * (h3 + 3 * h * h * (x - hx[1]) + 3 * h * (x - hx[1]) * (x - hx[1]) - 3 * (x - hx[1]) * (x - hx[1]) * (x - hx[1]));
-	else if (x > hx[2] && x <= hx[3])
-		return 1 / h3 * (h3 + 3 * h * h * (hx[3] - x) + 3 * h * (hx[3] - x) * (hx[3] - x) - 3 * (hx[3] - x) * (hx[3] - x) * (hx[3] - x));
-	else			/* if (x > hx[3]) && (x <= hx[4]) */
-		return 1 / h3 * (hx[4] - x) * (hx[4] - x) * (hx[4] - x);
+fi(int i, double x) {
+	double H = 1;
+	double tmp1 = 0;
+	double tmp2 = 0;
+	for (int j = 0; j < i; j++) {
+		if (j%2 == 0)
+			tmp1 = H;
+		else
+			tmp2 = H;
+		H *= 2*x;
+		if (j%2 == 1)
+			H -= 2*j*tmp1;
+		else
+			H -= 2*j*tmp2;
+	}
+	return H;
 }
 
 /* Pierwsza pochodna fi */
 double
-dfi(double a, double b, int n, int i, double x)
-{
-	double		h = (b - a) / (n - 1);
-	double		h3 = h * h * h;
-	int		hi         [5] = {i - 2, i - 1, i, i + 1, i + 2};
-	double		hx      [5];
-	int		j;
+dfi(double a, double b, int n, int i, double x) {
+	double dx = (b-a)/(n-1);
+	double dH = fi(i, x+dx) - fi(i, x-dx);
+	return dH/(2*dx);
 
-	for (j = 0; j < 5; j++)
-		hx[j] = a + h * hi[j];
-
-	if ((x < hx[0]) || (x > hx[4]))
-		return 0;
-	else if (x >= hx[0] && x <= hx[1])
-		return 3 / h3 * (x - hx[0]) * (x - hx[0]);
-	else if (x > hx[1] && x <= hx[2])
-		return 1 / h3 * (3 * h * h + 6 * h * (x - hx[1]) - 9 * (x - hx[1]) * (x - hx[1]));
-	else if (x > hx[2] && x <= hx[3])
-		return 1 / h3 * (-3 * h * h - 6 * h * (hx[3] - x) + 9 * (hx[3] - x) * (hx[3] - x));
-	else			/* if (x > hx[3]) && (x <= hx[4]) */
-		return -3 / h3 * (hx[4] - x) * (hx[4] - x);
 }
 
 /* Druga pochodna fi */
 double
-d2fi(double a, double b, int n, int i, double x)
-{
-	double		h = (b - a) / (n - 1);
-	double		h3 = h * h * h;
-	int		hi         [5] = {i - 2, i - 1, i, i + 1, i + 2};
-	double		hx      [5];
-	int		j;
-
-	for (j = 0; j < 5; j++)
-		hx[j] = a + h * hi[j];
-
-	if ((x < hx[0]) || (x > hx[4]))
-		return 0;
-	else if (x >= hx[0] && x <= hx[1])
-		return 6 / h3 * (x - hx[0]);
-	else if (x > hx[1] && x <= hx[2])
-		return 1 / h3 * (6 * h - 18 * (x - hx[1]));
-	else if (x > hx[2] && x <= hx[3])
-		return 1 / h3 * (6 * h  -18 * (hx[3] - x));
-	else			/* if (x > hx[3]) && (x <= hx[4]) */
-		return 6 / h3 * (hx[4] - x);
+d2fi(double a, double b, int n, int i, double x) {
+	double dx = (b-a)/(n-1);
+	double dH = dfi(a, b, n, i, x+dx) - dfi(a, b, n, i, x-dx);
+	return dH/(2*dx);
 }
 
 /* Trzecia pochodna fi */
 double
-d3fi(double a, double b, int n, int i, double x)
-{
-	double		h = (b - a) / (n - 1);
-	double		h3 = h * h * h;
-	int		hi         [5] = {i - 2, i - 1, i, i + 1, i + 2};
-	double		hx      [5];
-	int		j;
-
-	for (j = 0; j < 5; j++)
-		hx[j] = a + h * hi[j];
-
-	if ((x < hx[0]) || (x > hx[4]))
-		return 0;
-	else if (x >= hx[0] && x <= hx[1])
-		return 6 / h3;
-	else if (x > hx[1] && x <= hx[2])
-		return -18 / h3;
-	else if (x > hx[2] && x <= hx[3])
-		return 18 / h3;
-	else			/* if (x > hx[3]) && (x <= hx[4]) */
-		return -6 / h3;
+d3fi(double a, double b, int n, int i, double x) {
+	double dx = (b-a)/(n-1);
+	double dH = d2fi(a, b, n, i, x+dx) - d2fi(a, b, n, i, x-dx);
+	return dH/(2*dx);
 }
 
 /* Pomocnicza f. do rysowania bazy */
@@ -158,11 +103,11 @@ make_spl(points_t * pts, spline_t * spl)
 		FILE           *tst = fopen("debug_base_plot.txt", "w");
 		double		dx = (b - a) / (TESTBASE - 1);
 		for( j= 0; j < nb; j++ )
-			xfi( a, b, nb, j, tst );
+			xfi( j, tst );
 		for (i = 0; i < TESTBASE; i++) {
 			fprintf(tst, "%g", a + i * dx);
 			for (j = 0; j < nb; j++) {
-				fprintf(tst, " %g", fi  (a, b, nb, j, a + i * dx));
+				fprintf(tst, " %g", fi  (j, a + i * dx));
 				fprintf(tst, " %g", dfi (a, b, nb, j, a + i * dx));
 				fprintf(tst, " %g", d2fi(a, b, nb, j, a + i * dx));
 				fprintf(tst, " %g", d3fi(a, b, nb, j, a + i * dx));
@@ -176,10 +121,10 @@ make_spl(points_t * pts, spline_t * spl)
 	for (j = 0; j < nb; j++) {
 		for (i = 0; i < nb; i++)
 			for (k = 0; k < pts->n; k++)
-				add_to_entry_matrix(eqs, j, i, fi(a, b, nb, i, x[k]) * fi(a, b, nb, j, x[k]));
+				add_to_entry_matrix(eqs, j, i, fi(i, x[k]) * fi(j, x[k]));
 
 		for (k = 0; k < pts->n; k++)
-			add_to_entry_matrix(eqs, j, nb, y[k] * fi(a, b, nb, j, x[k]));
+			add_to_entry_matrix(eqs, j, nb, y[k] * fi(j, x[k]));
 	}
 
 #ifdef DEBUG
@@ -204,7 +149,7 @@ make_spl(points_t * pts, spline_t * spl)
 			spl->f3[i] = 0;
 			for (k = 0; k < nb; k++) {
 				double		ck = get_entry_matrix(eqs, k, nb);
-				spl->f[i]  += ck * fi  (a, b, nb, k, xx);
+				spl->f[i]  += ck * fi  (k, xx);
 				spl->f1[i] += ck * dfi (a, b, nb, k, xx);
 				spl->f2[i] += ck * d2fi(a, b, nb, k, xx);
 				spl->f3[i] += ck * d3fi(a, b, nb, k, xx);
@@ -223,7 +168,7 @@ make_spl(points_t * pts, spline_t * spl)
 			double d3yi= 0;
 			double xi= a + i * dx;
 			for( k= 0; k < nb; k++ ) {
-							yi += get_entry_matrix(eqs, k, nb) * fi(a, b, nb, k, xi);
+							yi += get_entry_matrix(eqs, k, nb) * fi(k, xi);
 							dyi += get_entry_matrix(eqs, k, nb) * dfi(a, b, nb, k, xi);
 							d2yi += get_entry_matrix(eqs, k, nb) * d2fi(a, b, nb, k, xi);
 							d3yi += get_entry_matrix(eqs, k, nb) * d3fi(a, b, nb, k, xi);
